784bf11e0e4340fbf00188b4cf4f05486789a1f7
JosepeDev <yosefd99@gmail.com>
2021-05-10 22:41:20 +0300

    Update 2.1.0
    - Added more options to the sound object
    - Now there is a Global and a Local AudioManager
     - A local audio manager will be used when you want to play audio on a specific object in the world
     - A global audio manager will be used to play UI audio, and sounds that don't need to be on a specific object

---
+++ GlobalAudioPlayerEditor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Sounder
+{
+    [CustomEditor(typeof(GlobalAudioPlayer))]
+    public class GlobalAudioPlayerEditor : Editor
+    {
+        readonly string btxt_init = "Initialize";
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            GlobalAudioPlayer player = (GlobalAudioPlayer)target;
+
+            if (!player.initializeOnAwake)
+            {
+                if (GUILayout.Button(btxt_init))
+                {
+                    player.Initialize();
+                }
+            }
+
+            if (player.cashedInitializeOnAwake != player.initializeOnAwake)
+            {
+                player.ResetAudioSourcesContainer();
+                player.cashedInitializeOnAwake = player.initializeOnAwake;
+            }
+        }
+    }
+}

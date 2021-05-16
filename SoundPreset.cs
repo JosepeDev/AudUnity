8d00a36a073842ce259e3a483f31b749a8330906
JosepeDev <yosefd99@gmail.com>
2021-05-16 20:29:10 +0300

    Deleted Useless scripts

--- SoundPreset.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.Audio;
-
-[CreateAssetMenu(menuName = "SoundPreset")]
-public class SoundPreset : ScriptableObject
-{
-    [Tooltip("The sound contained in the preset.")]
-    public Sound sound;
-}
